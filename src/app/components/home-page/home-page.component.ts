c9dcb36240411e89861a6d4580ac65dbbc05b6c6
Ali Tarek <tali6184@gmail.com>
2023-01-09 04:29:43 +0200

    Third Commit

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -9,17 +9,17 @@ export class HomePageComponent implements OnInit {
 
   blog= [
     {
-      image: '../../../assets/img/blog/blog-1.jpg',
+      image: 'assets/img/blog/blog-1.jpg',
       title: 'Cooking tips make cooking simple',
       content: 'Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat'
     },
     {
-      image: '../../../assets/img/blog/blog-2.jpg',
+      image: 'assets/img/blog/blog-2.jpg',
       title: '6 ways to prepare breakfast for 30',
       content: 'Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat'
     },
     {
-      image: '../../../assets/img/blog/blog-3.jpg',
+      image: 'assets/img/blog/blog-3.jpg',
       title: 'Visit the clean farm in the US',
       content: 'Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat'
     }    
